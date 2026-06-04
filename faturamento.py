cfea8ba41ea4b2dd657a82aefe134a1007e1ccf9
andremonte2 <andre.v.monte2@gmail.com>
2026-06-04 15:11:05 -0300

    Add faturamento.py with initial data structures

---
+++ faturamento.py
@@ -0,0 +1,10 @@
+import datetime
+notas_André = {"5 Janeiro": [135],
+                "8 Janeiro": [287],
+                "22 Janeiro": [951]
+            }
+
+faturamento1 = {"André": [1550,1780,4590,7815,0,0,0,0,0,0,0,0],
+                
+                "Geraldo Assis": [0,0,0,0,0,0,0,0,0,0,0,0]
+                }
